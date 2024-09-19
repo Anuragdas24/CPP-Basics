c77614473680f73dc134f9c5393c03e115094443
Anuragdas24 <147871494+Anuragdas24@users.noreply.github.com>
2024-09-19 22:08:39 +0530

    Add files via upload

---
+++ Recursion/LCM.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int getHCF(int num1, int num2)
+{
+    if (num1 == 0)
+        return num2;
+
+    if (num2 == 0)
+        return num1;
+
+    if (num1 == num2)
+        return num1;
+
+    if (num1 > num2)
+        return getHCF(num1 - num2, num2);
+
+    return getHCF(num1, num2 - num1);
+}
+
+int main()
+{
+    int num1 = 3, num2 = 21, HCF, LCM;
+
+    HCF = getHCF(num1, num2);
+
+    LCM = (num1 * num2) / HCF;
+
+    cout << "LCM of " << num1 << " and " << num2 << " is " << LCM;
+
+    return 0;
+}
